13a0ce1104fde8ead1af3a5b16b96d6699b91d6e
trishogen <patriciaogen@gmail.com>
2020-11-25 14:50:39 -0500

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,7 @@
-# code your solution here
+def power_digit_sum(base, power)
+  product = base ** power
+  string_product = product.to_s
+  digits = string_product.split("")
+
+  return digits.collect{|digit| digit.to_i}.inject(:+)
+end
